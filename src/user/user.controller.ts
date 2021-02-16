b54a9e38dc2889cea80dcff07c1409c34d6e3652
Antonio Fernandes <imbitubadigital@gmail.com>
2021-02-16 17:50:27 -0300

    add common module

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,40 @@
 import { UserService } from './user.service';
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  ClassSerializerInterceptor,
+  Controller,
+  Get,
+  Param,
+  Post,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
 import { User } from './models/user.entity';
+import * as bcrypt from 'bcryptjs';
+import { UserCreateDto } from './models/user-create.dto';
+import { AuthGuard } from 'src/auth/auth.guard';
 
+@UseInterceptors(ClassSerializerInterceptor)
+@UseGuards(AuthGuard)
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
   @Get()
   async all(): Promise<User[]> {
     return this.userService.all();
+  }
+
+  @Post()
+  async create(@Body() body: UserCreateDto): Promise<User> {
+    const passwordHash = await bcrypt.hash('123456', 12);
+    return this.userService.create({
+      ...body,
+      password: passwordHash,
+    });
+  }
+
+  @Get(':id')
+  async getUser(@Param('id') id: string) {
+    return this.userService.findOne({ id: Number(id) });
   }
 }
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { CommonModule } from 'src/common/common.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]), CommonModule],
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService],
