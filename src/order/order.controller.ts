f21c4d264564ca1e0c41a2c5ebb3bfc5f12523fb
Antonio Fernandes <imbitubadigital@gmail.com>
2021-02-18 14:57:33 -0300

    csv and chart

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,4 +1,69 @@
-import { Controller } from '@nestjs/common';
+import { OrderItem } from './order-item.entity';
+import { Order } from './order.entity';
+import { AuthGuard } from './../auth/auth.guard';
+import {
+  ClassSerializerInterceptor,
+  Controller,
+  Get,
+  ParseArrayPipe,
+  Post,
+  Query,
+  Res,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
+import { OrderService } from './order.service';
+import { Response } from 'express';
+import { Parser } from 'json2csv';
 
-@Controller('order')
-export class OrderController {}
+@UseInterceptors(ClassSerializerInterceptor)
+@UseGuards(AuthGuard)
+@Controller()
+export class OrderController {
+  constructor(private readonly orderService: OrderService) {}
+  @Get('orders')
+  async all(@Query('page') page = 1) {
+    return this.orderService.paginate(Number(page), ['order_items']);
+  }
+
+  @Post('export')
+  async export(@Res() res: Response) {
+    const parse = new Parser({
+      fields: ['ID', 'Name', 'Email', 'Product Title', 'Price', 'Quantity'],
+    });
+
+    const orders = await this.orderService.all(['order_items']);
+    const json = [];
+    orders.forEach((o: Order) => {
+      json.push({
+        ID: o.id,
+        Name: o.name,
+        Email: o.email,
+        'Product Title': '',
+        Price: '',
+        Quantity: '',
+      });
+
+      o.order_items.forEach((i: OrderItem) => {
+        json.push({
+          ID: '',
+          Name: '',
+          Email: '',
+          'Product Title': i.product_title,
+          Price: i.price,
+          Quantity: i.quantity,
+        });
+      });
+    });
+
+    const csv = parse.parse(json);
+    res.header('Content-Type', 'text/csv');
+    res.attachment('orders.csv');
+    return res.send(csv);
+  }
+
+  @Get('chart')
+  async chart() {
+    return this.orderService.chart();
+  }
+}
--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -1,9 +1,14 @@
+import { OrderItem } from './order-item.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { OrderController } from './order.controller';
 import { OrderService } from './order.service';
+import { Order } from './order.entity';
+import { CommonModule } from 'src/common/common.module';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Order, OrderItem]), CommonModule],
   controllers: [OrderController],
-  providers: [OrderService]
+  providers: [OrderService],
 })
 export class OrderModule {}
