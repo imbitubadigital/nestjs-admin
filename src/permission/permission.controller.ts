2a46e4777f92d4570ee23442703669455feb441b
Antonio Fernandes <imbitubadigital@gmail.com>
2021-02-18 16:47:09 -0300

    Custom decoration

--- src/permission/permission.controller.ts
+++ src/permission/permission.controller.ts
@@ -1,12 +1,14 @@
 import { PermissionService } from './permission.service';
 import { Controller, Get } from '@nestjs/common';
 import { Permission } from './permission.entity';
+import { HasPermission } from './has-permisson.dedorator';
 
 @Controller('permissions')
 export class PermissionController {
   constructor(private readonly permissionService: PermissionService) {}
 
   @Get()
+  @HasPermission('view_permissions')
   async all(): Promise<Permission[]> {
     return this.permissionService.all();
   }
